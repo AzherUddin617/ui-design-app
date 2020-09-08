2d128d3edd7c9ed3026a92b62e481cc45d31f6ce
Azher Uddin <azher305uddin@gmail.com>
2020-09-08 11:40:07 +0600

    Added fontawosome and replesed with material-ui

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import classes from './Toolbar.module.scss';
 
-import NearMeIcon from '@material-ui/icons/NearMe';
-import SquareIcon from '@material-ui/icons/CropSquare';
-import CircleIcon from '@material-ui/icons/RadioButtonUnchecked';
-import LineIcon from '@material-ui/icons/Remove';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSlash } from '@fortawesome/free-solid-svg-icons';
+import { faMousePointer } from '@fortawesome/free-solid-svg-icons';
+import { faImages } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import TextFieldsIcon from '@material-ui/icons/TextFields';
-import ImageIcon from '@material-ui/icons/Image';
 
 const Toolbar = () => {
   return (
     <div className={classes.Toolbar}>
       <div className={classes.ToolItems}>
         <div className={classes.ToolItem}>
-          <NearMeIcon color="inherit" fontSize="inherit" style={{transform: 'rotate(-90deg)'}} />
+          <FontAwesomeIcon icon={faMousePointer} color="inherit" fontSize="inherit" transform="right-2" />
         </div>
         <div className={classes.ToolItem}>
-          <SquareIcon color="inherit" fontSize="inherit" style={{transform: 'scale(1.2)'}} />
+          <FontAwesomeIcon icon={faSquare} color="inherit" fontSize="inherit" />
         </div>
         <div className={classes.ToolItem}>
-          <CircleIcon color="inherit" fontSize="inherit" />
+          <FontAwesomeIcon icon={faCircle} color="inherit" fontSize="inherit" />
         </div>
         <div className={classes.ToolItem}>
-          <LineIcon color="inherit" fontSize="inherit" style={{transform: 'rotate(45deg) scaleX(1.5)'}} />
+          <FontAwesomeIcon icon={faSlash} color="inherit" fontSize="inherit" transform="shrink-4 left-2" />
         </div>
         <div className={classes.ToolItem}>
           <TextFieldsIcon color="inherit" fontSize="inherit" />
         </div>
         <div className={classes.ToolItem}>
-          <ImageIcon color="inherit" fontSize="inherit" />
+          <FontAwesomeIcon icon={faImages} color="inherit" fontSize="inherit" />
         </div>
       </div>
     </div>
